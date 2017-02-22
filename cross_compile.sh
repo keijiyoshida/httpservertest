0c6dd35359d932e366d5f5ae72569bb31188cd90
Keiji Yoshida <keijiyoshida.mail@gmail.com>
2017-02-22 14:56:29 +0900

    Update cross_compile.sh

--- cross_compile.sh
+++ cross_compile.sh
@@ -9,4 +9,3 @@ fi
 mkdir out
 
 docker run --rm -it -v "$PWD":/usr/local/go/src/github.com/keijiyoshida/httpservertest -w /usr/local/go/src/github.com/keijiyoshida/httpservertest golang:1.8 ./cross_compile_internal.sh $VERSION
-
