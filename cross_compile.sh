bd236fbed96b979f44ca9a0fe4cd30368b1e9442
Keiji Yoshida <keijiyoshida.mail@gmail.com>
2017-02-25 20:58:47 +0900

    Update cross_compile.sh

--- cross_compile.sh
+++ cross_compile.sh
@@ -8,4 +8,11 @@ fi
 
 mkdir out
 
-docker run --rm -it -v "$PWD":/usr/local/go/src/github.com/keijiyoshida/httpservertest -w /usr/local/go/src/github.com/keijiyoshida/httpservertest golang:1.8 ./cross_compile_internal.sh $VERSION
+for GOOS in darwin linux; do
+    for GOARCH in 386 amd64; do
+        mkdir out/httpservertest-$VERSION-$GOOS-$GOARCH
+        docker run --rm -it -v "$PWD":/usr/local/go/src/github.com/keijiyoshida/httpservertest -w /usr/local/go/src/github.com/keijiyoshida/httpservertest golang:1.8 /bin/bash -c "GOOS=$GOOS GOARCH=$GOARC go build -o out/httpservertest-$VERSION-$GOOS-$GOARCH/httpservertest cmd/httpservertest/main.go"
+        tar -zcf out/httpservertest-$VERSION-$GOOS-$GOARCH.tar.gz -C out httpservertest-$VERSION-$GOOS-$GOARCH
+        rm -rf out/httpservertest-$VERSION-$GOOS-$GOARCH
+    done
+done
